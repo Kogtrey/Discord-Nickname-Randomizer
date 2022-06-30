1a85ba16f361c1eac7452672d7ab2b5d19df0ba9
Kogtrey <46797493+Kogtrey@users.noreply.github.com>
2022-06-30 16:25:28 -0600

    Fixed optin command (Working)

--- commands/optin.js
+++ commands/optin.js
@@ -6,8 +6,15 @@ module.exports = {
             .setName('optin')
             .setDescription('Opts a user in to have their nickname randomly changed'),
     async execute(interaction,client){
-        if(client.userRepo.getByDiscordId(interaction.user.id)){
-            client.userRepo.create(interaction.user.username, interaction.user.id)
-        }
+        console.log(`User ${interaction.user.username} : ${interaction.user.id} sent /optin`)
+        client.userRepo.getByDiscordId(interaction.user.id)
+            .then((user) => {
+                if(!user){
+                    client.userRepo.create(interaction.user.username, interaction.user.id)
+                    interaction.reply(`User ${interaction.user.username} opted in!`)
+                } else {
+                    interaction.reply(`Usser ${interaction.user.username} already opted in (id: ${user.id})`)
+                }
+            })
     }
 }
