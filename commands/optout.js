bd23135c48712b741ce6dabaa113be5eccfe8a9a
noobis004 <noobis004@gmail.com>
2022-07-03 04:34:33 +0200

    Update optout.js

--- commands/optout.js
+++ commands/optout.js
@@ -7,14 +7,19 @@ module.exports = {
             .setDescription('Opts a user out of having their nickname randomly changed'),
     async execute(interaction,client){
         console.log(`User ${interaction.user.username} : ${interaction.user.id} sent /optout`)
-        client.userRepo.getByDiscordId(interaction.user.id)
-            .then((user) => {
-                if(user){
-                   client.userRepo.deleteByDiscordId(interaction.user.id)
-                   interaction.reply(`User ${interaction.user.username} has opted out.`)
-                } else {
-                    interaction.reply(`User ${interaction.user.username} is already opted out.`)
-                }
-            })
+       
+        await interaction.reply({
+            content: 'confirm in DM'
+        })
+        await interaction.user.id.send('it works')
+        //client.userRepo.getByDiscordId(interaction.user.id)
+        //     .then((user) => {
+        //        if(user){
+        //           client.userRepo.deleteByDiscordId(interaction.user.id)
+        //           interaction.reply(`User ${interaction.user.username} has opted out.`)
+        //        } else {
+        //            interaction.reply(`User ${interaction.user.username} is already opted out.`)
+        //        }
+        //    })
     }
 }
